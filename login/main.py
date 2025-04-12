6789a2ae6ed6999985c93b8895b8e7c0da493651
Hardik Kumar Sinha <hardikkumarsinha@gmail.com>
2025-04-12 17:51:31 +0530

    do we ball now

--- login/main.py
+++ login/main.py
@@ -1,6 +1,8 @@
 import sys
 from PySide6.QtWidgets import QApplication, QDialog, QMessageBox
 from login_ui import Ui_Dialog  # Import the UI class we generated
+from db import MongoConnection
+
 
 class LoginForm(QDialog):
     def __init__(self):
@@ -9,17 +11,29 @@ class LoginForm(QDialog):
         self.ui.setupUi(self)
         
         # Set window title
-        self.setWindowTitle("Login Form")
+        self.setWindowTitle("Login")
         
         # Connect the login button to a function
-        self.ui.loginbutton.clicked.connect(self.login)
+        self.ui.login_next.clicked.connect(self.login)
+        self.ui.register_2.clicked.connect(self.register)
         
         # Setup password input to show dots instead of characters
         self.ui.password_input.setEchoMode(self.ui.password_input.EchoMode.Password)
         
     def login(self):
-        email = self.ui.email_input.text()
+        user = self.ui.user_input.text()
         password = self.ui.password_input.text()
+
+
+        if user == "" and password == "":
+            QMessageBox.warning(self, "No data", "Please fill the data")
+        
+        else: 
+            if user == "":
+                QMessageBox.warning(self, "User not Found", "Please enter Username")
+            if password == "":
+                QMessageBox.warning(self, "Invalid Password", "Please enter Password")
+        #todo: change border color
 
 
         QMessageBox.information(self, "Success", "Login successful!")
@@ -35,6 +49,8 @@ class LoginForm(QDialog):
             # self.accept()  # This will close the dialog with an "accept" result
         else:
             QMessageBox.warning(self, "Login Failed", "Invalid email or password!")"""
+        
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
